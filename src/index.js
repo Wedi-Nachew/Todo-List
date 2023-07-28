98fd0058d4cce906a6d800238becadb770dfb615
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-28 20:56:58 +0300

    Create a form field to input info about the task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./style.css"
+
+// const add = document.querySelector(".header > button")
+// console.log(add)
+
+// const tasks = []
+// const taskInfo = {}
+// const Task=(title, description, dueTime, priority)=>{
+//     return{title, description, dueTime, priority}
+// }
+
+// function addTask(taskInfo){
+//     const newTask = Task(taskInfo.title, taskInfo.description, taskInfo.dueTime, taskInfo.priority)
+//     tasks.push(newTask);
+// }
+
+// add.addEventListener("click", ()=>{
+//     taskInfo.title = prompt("Enter Title")
+//     taskInfo.description = prompt("Enter Detailes")
+//     taskInfo.dueTime = prompt("Enter due time")
+//     taskInfo.priority = prompt("Enter the priority level of the task using 1-5 numbers")
+//     addTask(taskInfo)
+//     console.log(tasks)
+// })
